208c6f93fc175ae38c000db8c04ade54709f42bf
XDudiz <129982514+XDudiz@users.noreply.github.com>
2023-07-02 13:40:05 -0300

    Delete Cadastro directory

--- Cadastro/script.js
+++
@@ -1,6 +0,0 @@
-let nome = document.getElementById("Nome")
-let idade = document.getElementById("Idade")
-let endereço = document.getElementById("Endereço")
-let email = document.getElementById("Email")
-let senha = document.getElementById("Senha")
-
